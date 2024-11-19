463dd567d51f4b6e741d9908c714c6966250bccf
Jose Miguel Carvajal Jimenez <josemcj07@gmail.com>
2024-11-19 13:14:55 -0500

    added cors middleware

--- test-project/back-end/src/index.ts
+++ test-project/back-end/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, World!");
